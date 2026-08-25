f3ad5d5767c32418bc6773c9958aff1a9c2099db
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-08-25 10:14:32 -0300

    Add- Estrutura cadastro cliente

--- public_u/cad_cliente.php
+++ public_u/cad_cliente.php
@@ -5,6 +5,31 @@ include 'conexao.php';
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 
-$sql = "INSERT"
+$sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
 
-?>
+if (mysqli_query($conexao, $sql)) {
+    echo "Cliente cadastrado com sucesso!";
+} else {
+    echo "Erro ao cadastrar cliente: " . mysqli_error($conexao);
+}
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Usuario</title>
+</head>
+<body>
+    
+<label for="nome">Nome:</label>
+<input type="text" name="nome" id="nome" required>
+
+<label for="email">Email:</label>
+<input type="email" name="email" id="email" required>
+
+</body>
+</html>
+
